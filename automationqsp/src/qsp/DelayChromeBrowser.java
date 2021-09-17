b6fff3a6bae3ae70d1354578c60f6f105d6401fd
lenovo <lenovo@LAPTOP-AI9CD3N0>
2021-09-17 09:16:00 +0530

    hi

---
+++ automationqsp/src/qsp/DelayChromeBrowser.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DelayChromeBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		Thread.sleep(10000);
+		driver.close();
+	
+	}
+
+}
